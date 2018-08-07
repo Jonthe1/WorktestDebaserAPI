325c65182c6e918464452d1bb3d8e389bc50478d
Jonthe1 <29624918+Jonthe1@users.noreply.github>
2018-08-07 14:48:44 +0200

    Initial Commit. Finally got data on the screen!

---
+++ Debaser/Controllers/DebaserController.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using Debaser.Models;
+using System.Text;
+
+namespace Debaser.Controllers
+{
+    public class DebaserController : Controller
+    {
+        // GET: Debaser
+        public ActionResult Index()
+        {
+            var debaserData = GetData();
+            return View(debaserData);
+
+        }
+
+        private IEnumerable<DebaserData> GetData()
+        {
+            using (WebClient wc = new WebClient())
+            {
+                //var staticString = "[{\"EventId\":\"10024\",\"EventDate\":\"2013 - 01 - 04\",\"Club\":\"\",\"EventStatus\":\"\",\"Event\":\"The Royal Concept\",\"SubHead\":\" + Hyper Heart + Frida Sundemo\",\"TableBooking\":\"\",\"Description\":\"Det \u00e4r snart ett \u00e5r sedan The Royal Concept underh\u00f6ll tv\u00e5 fullsatta Debaser i Stockholm och Malm\u00f6.Sedan dess har bandet spelat \"Way out West\" och turnerat andra sidan atlanten hela h\u00f6sten. \r\n\r\nMusiken \u00e4r energisk, gitarrdriven, dansant, elektronisk pop som m\u00e5nga g\u00e5nger f\u00f6rknippats med band som \"Phoenix\" och \"The Strokes\". Med singlar som \"Damn\", \"World on fire\", \"Gimme Twice\" och \"Dddance\" har de h\u00f6rts frekvent p\u00e5 P3 under \u00e5ret som g\u00e5tt och hyllats bland annat tidningen Filter, Rolling Stone, MTV, Perez Hilton och inte minst av Will Smith. \r\n\r\nI somras var singeln \"Gimme Twice\" under tv\u00e5 veckor den mest adderade l\u00e5ten p\u00e5 radio i USA och musiken har under h\u00f6sten g\u00e5tt att h\u00f6ras i bland annat \"Greys Anatomy\", \"Grimm!\", \"FIFA 13\" och Tim Burtons film \"Frankenweenie\". \r\n\r\nEP:n finns att h\u00f6ra h\u00e4r: (The Royal Concept \u2013 The Royal Concept EP) och det efterl\u00e4ngtade debutalbumet sl\u00e4pps i v\u00e5r. The Royal Concept \u00e4r en liveakt du inte vill missa!\r\n\r\nBOKA BORD! Har ni biljett till spelningen och bokar bord i v\u00e5r restaurang s\u00e5 smiter ni \u00e4ven f\u00f6re i k\u00f6n!\r\n\r\n<iframe src=\"http:\/\/www.tinybooking.com\/booking\/index\/debaser-medis?lang=english\" framborder=\"0\" border=\"0\" scrolling=\"no\" width=\"335\" height=\"330\"  \/><\/iframe>","Age":"20 \u00e5r","Open":"19-03","Admission":"125 kr + f\u00f6rk\u00f6psavgift. Biljetter finns hos Tickster, Bengans, Sound Pollution, Record Hunter, Kulturhuset\/Kulturdirekt.","Venue":"Medis","VenueSlug":"medis","Room":"Stora scenen","ImageDimensions":"618x412","Tags":[],"ImageUrl":"http:\/\/debaser.se\/img\/5744.jpg","TicketUrl":"https:\/\/secure.tickster.com\/Intro.aspx?ERC=LEPYHXKAJ3RG3HX","EventUrl":"http:\/\/debaser.se\/kalender\/10024\/"}]"
+                var jsonString = wc.DownloadString("http://debaser.se/debaser/api/?version=2&method=getevents&venue=medis&from=20130101&to=20140201&format=json");
+                byte[] bytes = Encoding.Default.GetBytes(jsonString);
+
+                var newJsonString = jsonString = Encoding.UTF8.GetString(bytes);
+
+                var debaserData1 = JsonConvert.DeserializeObject<IEnumerable<DebaserData>>(newJsonString);
+                return debaserData1;
+            }
+
+        }
+
+        private static T _download_serialized_json_data<T>(string url) where T : new()
+        {
+            using (var w = new WebClient())
+            {
+                var json_data = string.Empty;
+                // attempt to download JSON data as a string
+                try
+                {
+                    json_data = w.DownloadString(url);
+                }
+                catch (Exception) { }
+                // if string with JSON data is not empty, deserialize it to class and return its instance 
+                return !string.IsNullOrEmpty(json_data) ? JsonConvert.DeserializeObject<T>(json_data) : new T();
+            }
+        }
+    }
+}
---
+++ Debaser/Models/DebaserDataXML.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Debaser.Models
+{
+    public class DebaserDataXML
+    {
+
+        // NOTE: Generated code may require at least .NET Framework 4.5 or .NET Core/Standard 2.0.
+        /// <remarks/>
+        [System.SerializableAttribute()]
+        [System.ComponentModel.DesignerCategoryAttribute("code")]
+        [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+        [System.Xml.Serialization.XmlRootAttribute(Namespace = "", IsNullable = false)]
+        public partial class xml
+        {
+
+            private xmlEvent[] eventField;
+
+            /// <remarks/>
+            [System.Xml.Serialization.XmlElementAttribute("event")]
+            public xmlEvent[] @event
+            {
+                get
+                {
+                    return this.eventField;
+                }
+                set
+                {
+                    this.eventField = value;
+                }
+            }
+        }
+
+        /// <remarks/>
+        [System.SerializableAttribute()]
+        [System.ComponentModel.DesignerCategoryAttribute("code")]
+        [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+        public partial class xmlEvent
+        {
+
+            private string eventidField;
+
+            private string eventdateField;
+
+            private string clubField;
+
+            private string eventstatusField;
+
+            private string eventField;
+
+            private string subheadField;
+
+            private string tablebookingField;
+
+            private string descriptionField;
+
+            private string ageField;
+
+            private string openField;
+
+            private string admissionField;
+
+            private string venueField;
+
+            private string venueslugField;
+
+            private string roomField;
+
+            private string imagedimensionsField;
+
+            private string tagsField;
+
+            private string imageurlField;
+
+            private string ticketurlField;
+
+            private string eventurlField;
+
+            /// <remarks/>
+            public string eventid
+            {
+                get
+                {
+                    return this.eventidField;
+                }
+                set
+                {
+                    this.eventidField = value;
+                }
+            }
+
+            /// <remarks/>
+            public string eventdate
+            {
+                get
+                {
+                    return this.eventdateField;
+                }
+                set
+                {
+                    this.eventdateField = value;
+                }
+            }
+
+            /// <remarks/>
+            public string club
+            {
+                get
+                {
+                    return this.clubField;
+                }
+                set
+                {
+                    this.clubField = value;
+                }
+            }
+
+            /// <remarks/>
+            public string eventstatus
+            {
+                get
+                {
+                    return this.eventstatusField;
+                }
+                set
+                {
+                    this.eventstatusField = value;
+                }
+            }
+
+            /// <remarks/>
+            public string @event
+            {
+                get
+                {
+                    return this.eventField;
+                }
+                set
+                {
+                    this.eventField = value;
+                }
+            }
+
+            /// <remarks/>
+            public string subhead
+            {
+                get
+                {
+                    return this.subheadField;
+                }
+                set
+                {
+                    this.subheadField = value;
+                }
+            }
+
+            /// <remarks/>
+            public string tablebooking
+            {
+                get
+                {
+                    return this.tablebookingField;
+                }
+                set
+                {
+                    this.tablebookingField = value;
+                }
+            }
+
+            /// <remarks/>
+            public string description
+            {
+                get
+                {
+                    return this.descriptionField;
+                }
+                set
+                {
+                    this.descriptionField = value;
+                }
+            }
+
+            /// <remarks/>
+            public string age
+            {
+                get
+                {
+                    return this.ageField;
+                }
+                set
+                {
+                    this.ageField = value;
+                }
+            }
+
+            /// <remarks/>
+            public string open
+            {
+                get
+                {
+                    return this.openField;
+                }
+                set
+                {
+                    this.openField = value;
+                }
+            }
+
+            /// <remarks/>
+            public string admission
+            {
+                get
+                {
+                    return this.admissionField;
+                }
+                set
+                {
+                    this.admissionField = value;
+                }
+            }
+
+            /// <remarks/>
+            public string venue
+            {
+                get
+                {
+                    return this.venueField;
+                }
+                set
+                {
+                    this.venueField = value;
+                }
+            }
+
+            /// <remarks/>
+            public string venueslug
+            {
+                get
+                {
+                    return this.venueslugField;
+                }
+                set
+                {
+                    this.venueslugField = value;
+                }
+            }
+
+            /// <remarks/>
+            public string room
+            {
+                get
+                {
+                    return this.roomField;
+                }
+                set
+                {
+                    this.roomField = value;
+                }
+            }
+
+            /// <remarks/>
+            public string imagedimensions
+            {
+                get
+                {
+                    return this.imagedimensionsField;
+                }
+                set
+                {
+                    this.imagedimensionsField = value;
+                }
+            }
+
+            /// <remarks/>
+            public string tags
+            {
+                get
+                {
+                    return this.tagsField;
+                }
+                set
+                {
+                    this.tagsField = value;
+                }
+            }
+
+            /// <remarks/>
+            public string imageurl
+            {
+                get
+                {
+                    return this.imageurlField;
+                }
+                set
+                {
+                    this.imageurlField = value;
+                }
+            }
+
+            /// <remarks/>
+            public string ticketurl
+            {
+                get
+                {
+                    return this.ticketurlField;
+                }
+                set
+                {
+                    this.ticketurlField = value;
+                }
+            }
+
+            /// <remarks/>
+            public string eventurl
+            {
+                get
+                {
+                    return this.eventurlField;
+                }
+                set
+                {
+                    this.eventurlField = value;
+                }
+            }
+        }
+
+
+    }
+}
